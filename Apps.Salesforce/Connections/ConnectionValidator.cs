41326ec625788642a715c85cc993d6c2832869a3
bZverok <bohdan.zverok@blackbird.io>
2023-09-25 10:28:48 +0300

    added connection validation

---
+++ Apps.Salesforce/Connections/ConnectionValidator.cs
@@ -0,0 +1,35 @@
+﻿using Blackbird.Applications.Sdk.Common.Authentication;
+using Blackbird.Applications.Sdk.Common.Connections;
+using RestSharp;
+
+namespace Apps.Salesforce.Crm.Connections;
+
+public class ConnectionValidator : IConnectionValidator
+{
+    public async ValueTask<ConnectionValidationResponse> ValidateConnection(
+        IEnumerable<AuthenticationCredentialsProvider> authProviders, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var client = new SalesforceClient(authProviders);
+
+            var query = "SELECT FIELDS(ALL) FROM Contact LIMIT 200";
+            var request = new SalesforceRequest($"services/data/v57.0/query?q={query}", Method.Get, authProviders);
+
+            await client.ExecuteAsync(request, cancellationToken);
+
+            return new()
+            {
+                IsValid = true
+            };
+        }
+        catch (Exception ex)
+        {
+            return new()
+            {
+                IsValid = false,
+                Message = ex.Message
+            };
+        }
+    }
+}
--- Apps.Salesforce/Dtos/RecordIdDto.cs
+++ Apps.Salesforce/Dtos/RecordIdDto.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Apps.Salesforce.Crm.Dtos;
+﻿namespace Apps.Salesforce.Crm.Dtos;
 
 public class RecordIdDto
 {
